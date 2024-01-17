c60a8b0eb7087ec2f69ed45488b11d667e1e8df7
Gabriel Oliveira <gabriel.oliveirat@hotmail.com>
2024-01-17 18:48:58 -0300

    removing unused code and code to remove lines after upload to s3

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,9 @@
 
 from page import manipulate_html_file
+
 def lambda_handler():
-    # TODO implement
 
     return manipulate_html_file(['Belford Roxo', 'Mesquita', 'Nova Iguaçu', 'Nilópolis'])
+
 
 lambda_handler()
--- page.py
+++ page.py
@@ -21,6 +21,17 @@ def manipulate_html_file(params):
                 if not lines[i].strip():
                     lines[i] = f"    <div id='weather-info'>\n        <h2>{weather['city_name']}</h2><img src='{weather['weather_icon']}'>\n        <p>Temperature: {weather['temp']} °C</p>\n        <p>Weather: {weather['weather']} - {weather['weather_description']}</p>\n        <p>Min. Temperature: {weather['temp_min']} °C</p>\n        <p>Max. Temperature: {weather['temp_max']} °C</p>\n        <p>Humidity: {weather['humidity']}%</p>\n        <p>Last update: {datetime.now()}</p>\n    </div>\n\n"
             # Write the modified content back to the file
-        with open(file_path, 'w') as file:
-            file.writelines(lines)
+            with open(file_path, 'w') as file:
+                file.writelines(lines)
+
+    # Read the content of the file to delete the infos after send to S3
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Identify the range of lines to delete
+    del lines[42:78]
+
+    # Write the updated content back to the file
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
 
